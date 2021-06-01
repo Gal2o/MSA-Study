d5869b57f3bae491e630d5b2f4fd4fa22b096731
최현민 <hunmin8788@naver.com>
2021-06-02 01:48:04 +0900

    Eureka, Zuul, Spring Cloud

---
+++ spring-cloud/spring-cloud-gateway/src/main/java/com/example/springcloudgateway/filter/CustomFilter.java
@@ -0,0 +1,38 @@
+package com.example.springcloudgateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+    public CustomFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // Custom Pre Filter
+        return (exchange, chain) -> {
+            // http.server.reactive 클래스를 사용해야 webflux 기능을 사용할 수 있다.
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Custom PRE filter: request id -> {}", request.getId());
+
+            // Custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("Custom POST filter: response code -> {}", response.getStatusCode());
+            }));
+        };
+    }
+
+    public static class Config {
+        // Put the configuration properties
+    }
+}
